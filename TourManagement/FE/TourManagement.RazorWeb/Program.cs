4ad93c4ddbe71df1313a6fc1b38ce0118ac08380
Ngoc Do <email@gmail.com>
2026-06-24 23:23:28 +0700

    Downgrade TourManagement.RazorWeb to .NET 8 to unify framework versions

--- TourManagement/FE/TourManagement.RazorWeb/Program.cs
+++ TourManagement/FE/TourManagement.RazorWeb/Program.cs
@@ -34,8 +34,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.UseStaticFiles();
+app.MapRazorPages();
 
 app.Run();
